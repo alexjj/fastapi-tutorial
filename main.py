d83eb2b5623023a087bf2a690a423a98bf4b30a3
Alex Johnstone <alexjj@gmail.com>
2020-12-21 19:22:37 +0000

    started chapter 5

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import fastapi
 import uvicorn
 
@@ -8,34 +6,8 @@ api = fastapi.FastAPI()
 
 @api.get('/')
 def index():
-    body = "<html>" \
-           "<body style='padding: 10px;'>" \
-           "<h1>Welcome to the API</h1>" \
-           "<div>" \
-           "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>" \
-           "</div>" \
-           "</body>" \
-           "</html>"
-
-    return fastapi.responses.HTMLResponse(content=body)
+    return "Hello Weather App!"
 
 
-@api.get('/api/calculate')
-def calculate(x: int, y: int, z: Optional[int] = None):
-    if z == 0:
-        return fastapi.responses.JSONResponse(content={"error": "ERROR: Z cannot be zero."}, status_code=400)
-
-    value = (x + y)
-
-    if z is not None:
-        value /= z
-
-    return {
-        'x': x,
-        'y': y,
-        'z': z,
-        'value': value
-    }
-
-
-uvicorn.run(api, port=8000, host="127.0.0.1")
+if __name__ == '__main__':
+    uvicorn.run(api, port=8000, host="127.0.0.1")
